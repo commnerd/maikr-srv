b14ca29e01ee0e6624038499e5a3378d9aae777a
Michael J. Miller <commnerd@gmail.com>
2022-05-10 16:40:56 -0600

    Installed express.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
-const http = require("http");
-const host = 'localhost';
-const port = 8000;
+const express = require('express');
+const app = express();
 
-const requestListener = function (req, res) {
-    res.writeHead(200);
-    res.end("My first server!");
-};
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
 
-const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
-});
+app.listen(3000)
